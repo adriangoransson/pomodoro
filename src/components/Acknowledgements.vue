b12add2ecac46da639c63958826d1cca75295370
Adrian Göransson <adriangoransson@gmail.com>
2019-04-06 12:52:02 +0200

    Favicons and pwa options

--- src/components/Acknowledgements.vue
+++ src/components/Acknowledgements.vue
@@ -2,5 +2,7 @@
   <div>
     Thanks to:<br>
     https://freesound.org/people/FoolBoyMedia/sounds/264498/<br>
+
+    <a href="https://www.freepik.com/free-photos-vectors/food">Food vector created by bakar015 - www.freepik.com</a>
   </div>
 </template>
---
+++ vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  pwa: {
+    name: 'Pomodoro',
+    themeColor: '#fafafa',
+    msTileColor: '#fafafa',
+    appleMobileWebAppCapable: 'yes',
+  },
+};
