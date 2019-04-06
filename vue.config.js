6d45ad98968e7916a717da913384546c893f5cb2
Adrian Göransson <adriangoransson@gmail.com>
2019-04-06 14:37:37 +0200

    Revert "Add prerender plugin for production builds"
    This reverts commit c91b9714de00900fce24f0eccbe111e55b948b68.

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,8 @@
-const path = require('path');
-const PrerenderSPAPlugin = require('prerender-spa-plugin');
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = {
-    pwa: {
-      name: 'Pomodoro',
-      themeColor: '#fafafa',
-      msTileColor: '#fafafa',
-      appleMobileWebAppCapable: 'yes',
-    },
-    configureWebpack: {
-      plugins: [
-        new PrerenderSPAPlugin({
-          staticDir: path.join(__dirname, 'dist'),
-          routes: ['/'],
-        }),
-      ],
-    },
-  };
-}
+module.exports = {
+  pwa: {
+    name: 'Pomodoro',
+    themeColor: '#fafafa',
+    msTileColor: '#fafafa',
+    appleMobileWebAppCapable: 'yes',
+  },
+};
