8ffe0a674878af57f8ba07e29c16b637c217ca53
Adrian Göransson <adriangoransson@gmail.com>
2019-04-06 17:20:00 +0200

    Updated iconPaths for service worker

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,12 @@ module.exports = {
     themeColor: '#fafafa',
     msTileColor: '#fafafa',
     appleMobileWebAppCapable: 'yes',
+    iconPaths: {
+      favicon32: '/favicon-32x32.png',
+      favicon16: '/favicon-16x16.png',
+      appleTouchIcon: '/apple-touch-icon-152x152.png',
+      maskIcon: '/safari-pinned-tab.svg',
+      msTileImage: '/mstile-150x150',
+    },
   },
 };
