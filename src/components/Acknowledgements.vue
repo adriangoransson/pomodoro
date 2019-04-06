961bac71ea2434b03eb0442dfbecb5694af13ecb
Adrian Göransson <adriangoransson@gmail.com>
2019-04-06 13:40:26 +0200

    Delete acknowledgements component
    Credits are in the readme

--- src/components/Acknowledgements.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    Thanks to:<br>
-    https://freesound.org/people/FoolBoyMedia/sounds/264498/<br>
-
-    <a href="https://www.freepik.com/free-photos-vectors/food">Food vector created by bakar015 - www.freepik.com</a>
-  </div>
-</template>
