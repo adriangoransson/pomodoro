c91b9714de00900fce24f0eccbe111e55b948b68
Adrian Göransson <adriangoransson@gmail.com>
2019-04-06 14:31:07 +0200

    Add prerender plugin for production builds

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,21 @@
-module.exports = {
-  pwa: {
-    name: 'Pomodoro',
-    themeColor: '#fafafa',
-    msTileColor: '#fafafa',
-    appleMobileWebAppCapable: 'yes',
-  },
-};
+const path = require('path');
+const PrerenderSPAPlugin = require('prerender-spa-plugin');
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports = {
+    pwa: {
+      name: 'Pomodoro',
+      themeColor: '#fafafa',
+      msTileColor: '#fafafa',
+      appleMobileWebAppCapable: 'yes',
+    },
+    configureWebpack: {
+      plugins: [
+        new PrerenderSPAPlugin({
+          staticDir: path.join(__dirname, 'dist'),
+          routes: ['/'],
+        }),
+      ],
+    },
+  };
+}
