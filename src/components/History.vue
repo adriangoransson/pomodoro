89fdcde8eaac3c983a0baee29f1c9e732d4649df
Adrian Göransson <adriangoransson@gmail.com>
2019-04-06 11:08:16 +0200

    Refactor history entries into its own component

--- src/components/History.vue
+++ src/components/History.vue
@@ -1,69 +1,21 @@
 <template>
   <div class="history">
-    <div
-      v-for="(history, index) in $store.state.history"
+    <HistoryEntry
+      v-for="(entry, index) in $store.state.history"
       :key="index"
-      :class="history.type | slugType"
-      class="entry"
-    >
-      <h3 class="has-border-bottom">
-        {{ history.type | formattedType }}
-      </h3>
-      <pre v-if="history.text" class="has-border-bottom">{{ history.text }}</pre>
-    </div>
+      :entry="entry"
+    />
   </div>
 </template>
 
 <script>
-import { formattedType, slugType } from '@/utils';
+import HistoryEntry from './HistoryEntry.vue';
 
 export default {
   name: 'history',
 
-  filters: {
-    formattedType,
-    slugType,
+  components: {
+    HistoryEntry,
   },
 };
 </script>
-
-<style scoped>
-  .entry {
-    border-radius: var(--border-radius);
-    border-top: 1px solid var(--border-color);
-    border-right: 1px solid var(--border-color);
-    border-left: 1px solid var(--border-color);
-    margin: 10px 0;
-    background: #fff;
-    text-align: center;
-  }
-
-  .history h3 {
-    padding: 5px 20px;
-    margin: 0;
-    font-size: 1rem;
-    background: hsl(0, 0%, 99%);
-  }
-
-  .history pre {
-    padding: 5px;
-    margin: 0;
-    border-radius: var(--border-radius);
-  }
-
-  .has-border-bottom {
-    border-bottom: 1px solid var(--border-color);
-  }
-
-  .history .pomodoro h3 {
-    color: var(--pomodoro-color)
-  }
-
-  .history .short-break h3 {
-    color: var(--short-break-color)
-  }
-
-  .history .long-break h3 {
-    color: var(--long-break-color)
-  }
-</style>
---
+++ src/components/HistoryEntry.vue
@@ -0,0 +1,61 @@
+<template>
+  <div :class="entry.type | slugType" class="entry">
+    <h3 class="has-border-bottom">{{ entry.type | formattedType }}</h3>
+    <pre v-if="entry.text" class="has-border-bottom">{{ entry.text }}</pre>
+  </div>
+</template>
+
+<script>
+import { formattedType, slugType } from '@/utils';
+
+export default {
+  name: 'HistoryEntry',
+  props: ['entry'],
+
+  filters: {
+    formattedType,
+    slugType,
+  },
+};
+</script>
+
+<style scoped>
+  .entry {
+    border-radius: var(--border-radius);
+    border-top: 1px solid var(--border-color);
+    border-right: 1px solid var(--border-color);
+    border-left: 1px solid var(--border-color);
+    margin: 10px 0;
+    background: #fff;
+    text-align: center;
+  }
+
+  .entry h3 {
+    padding: 5px 20px;
+    margin: 0;
+    font-size: 1rem;
+    background: hsl(0, 0%, 99%);
+  }
+
+  .entry.pomodoro h3 {
+    color: var(--pomodoro-color)
+  }
+
+  .entry.short-break h3 {
+    color: var(--short-break-color)
+  }
+
+  .entry.long-break h3 {
+    color: var(--long-break-color)
+  }
+
+  .entry pre {
+    padding: 5px;
+    margin: 0;
+    border-radius: var(--border-radius);
+  }
+
+  .entry .has-border-bottom {
+    border-bottom: 1px solid var(--border-color);
+  }
+</style>
