b9ffe7569e7d32f26747084d99d9392df5a04b0d
Adrian Göransson <adriangoransson@gmail.com>
2019-04-08 11:49:03 +0200

    New attempt at clearing old service worker cache

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -5,10 +5,19 @@ import { register } from 'register-service-worker';
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
 
-    updated() {
-      caches.keys().then((name) => {
-        caches.delete(name);
+    updatefound(registration) {
+      registration.addEventListener('activate', (event) => {
+        console.log('New worker. Deleting old cache');
+        event.waitUntil(
+          caches
+            .keys()
+            .then(keys => Promise.all(keys.map(key => caches.delete(key)))),
+        );
       });
+    },
+
+    updated() {
+      console.log('Service worker updated');
     },
   });
 }
