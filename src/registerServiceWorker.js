943475d5956f693aa704af0611f489976369789a
Adrian Göransson <adriangoransson@gmail.com>
2019-04-08 11:51:49 +0200

    Remove service worker console logs.
    Firefox seems able to handle updates now.

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -7,17 +7,12 @@ if (process.env.NODE_ENV === 'production') {
 
     updatefound(registration) {
       registration.addEventListener('activate', (event) => {
-        console.log('New worker. Deleting old cache');
         event.waitUntil(
           caches
             .keys()
             .then(keys => Promise.all(keys.map(key => caches.delete(key)))),
         );
       });
-    },
-
-    updated() {
-      console.log('Service worker updated');
     },
   });
 }
